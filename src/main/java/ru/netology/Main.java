91dabfff53861f09daa6236f54b2532518cc8a02
Sudarev <soul7341@gmail.com>
2023-11-14 19:12:20 +0300

    Created Callable multithreading decision

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,44 +1,55 @@
 package ru.netology;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         String[] texts = new String[25];
-//        List<Thread> threads = new ArrayList<>();
+        List<Future> threads = new ArrayList<>();
+        final ExecutorService threadPool = Executors.newFixedThreadPool(7);
+
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
         for (String text : texts) {
-
-            Thread thread = new Thread(() -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
+            Callable<String> myCall = new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    int maxSize = 0;
+                    for (int i = 0; i < text.length(); i++) {
+                        for (int j = 0; j < text.length(); j++) {
+                            if (i >= j) {
+                                continue;
+                            }
+                            boolean bFound = false;
+                            for (int k = i; k < j; k++) {
+                                if (text.charAt(k) == 'b') {
+                                    bFound = true;
+                                    break;
+                                }
+                            }
+                            if (!bFound && maxSize < j - i) {
+                                maxSize = j - i;
                             }
                         }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-
                     }
+                    return (text.substring(0, 100) + " -> " + maxSize);
                 }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            });
-            threads.add(thread);
-            thread.start();
+            };
+            Future<String> task = threadPool.submit(myCall);
+            threads.add(task);
         }
-        for (Thread thread : threads) {
-            thread.join();
+        for (Future f : threads) {
+            try {
+                System.out.println(f.get());
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+            threadPool.shutdown();
         }
 //        long startTs = System.currentTimeMillis(); // start time
 //        for (String text : texts) {
